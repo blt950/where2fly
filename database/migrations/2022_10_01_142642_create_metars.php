aef790762af5bb0e44ae86b2d985adff9dfaba55
blt950 <blt950@users.noreply.github.com>
2022-10-01 17:52:27 +0200

    Added data structure for airports, rwy and metars

---
+++ database/migrations/2022_10_01_142642_create_metars.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('metars', function (Blueprint $table) {
+            $table->string('icao')->unique();
+            $table->timestamp('last_update');
+            $table->string('metar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('metars');
+    }
+};
