4448ac25bb849437e1727e5404d8960160aed926
blt950 <blt950@users.noreply.github.com>
2022-10-11 23:21:12 +0200

    Started on Metar modelling

--- app/Models/Metar.php
+++ app/Models/Metar.php
@@ -12,4 +12,50 @@ class Metar extends Model
 
     protected $guarded = [];
     protected $primaryKey = 'icao';
+
+    public function windAtAbove(int $knots){
+        $results = [];
+        if(preg_match('/(?!^...)(\d\d)(?=KT)/', $this->metar, $results)){
+            $airportWind = (int)$results[1];
+            return $airportWind >= $knots;
+        }
+        return false;
+    }
+
+    public function ceilingAtAbove(int $feet){
+        $results = [];
+        $metar = explode("RMK", $this->metar)[0];  // Remove the RMK parts
+        if(preg_match('/(BKN\d\d\d|OVC\d\d\d)/', $metar, $results)){
+            foreach($results as $r){
+                if((int)substr($r, 3)*100 <= $feet){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function foggy(){
+        // FG
+    }
+
+    public function heavyRain(){
+        // +RA +SHRA
+    }
+
+    public function heavySnow(){
+        // +SN
+    }
+
+    public function thunderstorm(){
+        // TS
+    }
+
+    public function rvrAtBelow(){
+        // VV
+    }
+
+    public function funClouds(){
+        // CB || TCU
+    }
 }
