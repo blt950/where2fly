8859a6b41519653c5d50c20ab17f05683a89672f
blt950 <blt950@users.noreply.github.com>
2022-10-16 13:10:19 +0200

    Added supportsAircraftCode()

--- app/Models/Airport.php
+++ app/Models/Airport.php
@@ -24,4 +24,38 @@ class Airport extends Model
         return $this->hasMany(AirportScore::class);
     }
 
+    public function supportsAircraftCode(string $code){
+
+        $reqRwyLength = 0;
+        switch($code){
+            case "A":
+                $reqRwyLength = 800;
+                break;
+            case "B":
+                $reqRwyLength = 2500;
+                break;
+            case "C":
+                $reqRwyLength = 3500;
+                break;
+            case "D":
+                $reqRwyLength = 3700;
+                break;
+            case "E":
+                $reqRwyLength = 5300;
+                break;
+            case "F":
+                $reqRwyLength = 6000;
+                break;
+            default:
+                $reqRwyLength = 0;
+        }
+
+        if($this->runways->where('closed', false)->where('length_ft', '>=', $reqRwyLength)->count()){
+            return true;
+        }
+
+        return false;
+
+    }
+
 }
