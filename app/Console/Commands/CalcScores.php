06efd6c41758fa1f77530783165f33bd63c2b000
blt950 <blt950@users.noreply.github.com>
2022-10-16 12:36:34 +0200

    Added crosswind calc for active rwy

--- app/Console/Commands/CalcScores.php
+++ app/Console/Commands/CalcScores.php
@@ -88,32 +88,40 @@ class CalcScores extends Command
                 $airportScore++;
             }
 
-            foreach($airport->runways as $runway){
-                if(!$runway->closed){
-
-                    // Check RVR at runways
-                    if(
-                        ( !empty($runway->le_ident) && $airport->metar->rvrAtBelow($runway->le_ident, 700) ) ||
-                        ( !empty($runway->he_ident) && $airport->metar->rvrAtBelow($runway->he_ident, 700) )
-                    ){
-                        AirportScore::create(['airport_id' => $airport->id, 'reason' => 'METAR_RVR', 'score' => 1]);
-                        $airportScore++;
-                    }
-
-                    // Check crosswind at runways
-                    if(!empty($airport->metar->wind_direction) && !empty($runway->le_heading) && !empty($runway->he_heading)){
-                        if($airport->metar->wind_speed >= 15){
-
-                            $runwayCrosswindComponent = abs($airport->metar->wind_speed * sin(($airport->metar->wind_direction - $runway->le_heading)/180*pi()));
-                            if($runwayCrosswindComponent >= 12){
-                                AirportScore::create(['airport_id' => $airport->id, 'reason' => 'METAR_CROSSWIND', 'score' => 1]);
-                                $airportScore++;
-                            }
-
-                        }
-                    }
-
+            $activeRunwayComponents = ['headwind' => 0, 'crosswind' => 0];
+            foreach($airport->runways->where('closed', false) as $runway){
+                // Check RVR at runways
+                if(
+                    ( !empty($runway->le_ident) && $airport->metar->rvrAtBelow($runway->le_ident, 700) ) ||
+                    ( !empty($runway->he_ident) && $airport->metar->rvrAtBelow($runway->he_ident, 700) )
+                ){
+                    AirportScore::create(['airport_id' => $airport->id, 'reason' => 'METAR_RVR', 'score' => 1]);
+                    $airportScore++;
                 }
+
+                // Calculate headwind component on active runway
+                if(!empty($airport->metar->wind_direction) && !empty($runway->le_heading) && !empty($runway->he_heading)){
+
+                    $headwindComponentLe = abs($airport->metar->wind_speed * cos(($airport->metar->wind_direction - $runway->le_heading)/180*3.14));
+                    $crosswindComponentLe = abs($airport->metar->wind_speed * sin(($airport->metar->wind_direction - $runway->le_heading)/180*pi()));
+
+                    $headwindComponentHe = abs($airport->metar->wind_speed * cos(($airport->metar->wind_direction - $runway->le_heading)/180*3.14));
+                    $crosswindComponentHe = abs($airport->metar->wind_speed * sin(($airport->metar->wind_direction - $runway->le_heading)/180*pi()));
+
+                    if($activeRunwayComponents['headwind'] < $headwindComponentLe){
+                        $activeRunwayComponents['headwind'] = $headwindComponentLe;
+                        $activeRunwayComponents['crosswind'] = $crosswindComponentLe;
+                    } else if($activeRunwayComponents['headwind'] < $headwindComponentHe){
+                        $activeRunwayComponents['headwind'] = $headwindComponentHe;
+                        $activeRunwayComponents['crosswind'] = $crosswindComponentHe;
+                    }
+                }
+            }
+
+            // Check if crosswind component is fun at active runway
+            if($airport->metar->wind_speed >= 15 && $activeRunwayComponents['crosswind'] > 12){
+                AirportScore::create(['airport_id' => $airport->id, 'reason' => 'METAR_CROSSWIND', 'score' => 1]);
+                 $airportScore++;
             }
 
             $airport->total_score = $airportScore;
