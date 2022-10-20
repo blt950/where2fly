d9a28cc04881446cca4dcd3eb561388e071a80c6
blt950 <blt950@users.noreply.github.com>
2022-10-20 18:31:55 +0200

    Create UpdateData.php

---
+++ app/Console/Commands/UpdateData.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class UpdateData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch all latest METARs from VATSIM';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $this->info("> Calling all relevant data update commands...");
+
+        Artisan::call('fetch:metar');
+        Artisan::call('fetch:vatsim');
+        Artisan::call('calc:score');
+
+        $this->info("> Done with all commands!");
+
+    }
+}
+
+
