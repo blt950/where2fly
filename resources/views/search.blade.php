c0ae32c68580ee2cdc1a33637718134ba21a1dd4
blt950 <blt950@users.noreply.github.com>
2022-10-31 22:56:50 +0100

    Hidden legend from desktops

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -92,7 +92,7 @@
             </table>
         </div>
 
-        <div>
+        <div class="d-block d-sm-none">
             <ul style="font-size: 0.7em; list-style-type: none;">
                 @foreach(\App\Http\Controllers\ScoreController::$score_types as $s)
                     <li>
