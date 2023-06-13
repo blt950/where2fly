5b687c521413f50e289826a1abebc2ba52543373
Daniel <blt950@users.noreply.github.com>
2023-06-13 22:54:04 +0200

    Removed discord alerts

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,12 +10,6 @@
     <main>
         <h1 class="mb-3 mt-5">Suggestion Results</h1>
 
-        <div class="alert alert-discord">
-            <p class="text-white mb-0">
-                <i class="fa-brands fa-discord"></i> Contribute with suggestions, bug reports and vote on new features in <a class="text-white" href="https://discord.gg/UkFg9Yy4gP" target="_blank">our Discord <i class="fas fa-up-right-from-square"></i></a>
-            </p>
-        </div>
-
         <div class="scroll-fade">
             <div class="table-responsive">
                 <table class="table table-hover text-start">
