8a06606067edc6fdea4c862b308ef44147dc4539
Daniel <blt950@users.noreply.github.com>
2023-07-22 17:06:38 +0200

    Added Discord to menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,6 +9,7 @@
             <a class="nav-link {{ Route::is('top*') ? 'active' : '' }}" href="{{ route('top') }}">Top List</a>
             <a class="nav-link {{ Route::is('changelog') ? 'active' : '' }}" href="{{ route('changelog') }}">Changelog</a>
             <a class="nav-link" href="https://forms.gle/wsP3s322LTP6oJog8" target="_blank">Feedback</a>
+            <a class="nav-link" href="https://discord.gg/UkFg9Yy4gP" target="_blank">Discord</a>
         </nav>
     </div>
 </header>
