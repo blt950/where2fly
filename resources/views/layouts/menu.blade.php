6ed309382c159c79177960be655597789068d710
Daniel <blt950@users.noreply.github.com>
2023-07-23 10:22:21 +0200

    Hide feedback button on mobile

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -8,7 +8,7 @@
             <a class="nav-link {{ Route::is('front') || Route::is('front.advanced') || Route::is('search*') ? 'active' : '' }}" href="{{ route('front') }}">Search</a>
             <a class="nav-link {{ Route::is('top*') ? 'active' : '' }}" href="{{ route('top') }}">Top List</a>
             <a class="nav-link {{ Route::is('changelog') ? 'active' : '' }}" href="{{ route('changelog') }}">Changelog</a>
-            <a class="nav-link" href="https://forms.gle/wsP3s322LTP6oJog8" target="_blank">Feedback</a>
+            <a class="nav-link d-none d-md-block" href="https://forms.gle/wsP3s322LTP6oJog8" target="_blank">Feedback</a>
             <a class="nav-link" href="https://discord.gg/UkFg9Yy4gP" target="_blank">Discord</a>
         </nav>
     </div>
