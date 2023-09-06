a372e923f0e6ba038b35d22399d52d4b777ccb78
Daniel <blt950@users.noreply.github.com>
2023-09-06 22:27:16 +0200

    Started on API functions

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -9,57 +9,83 @@ use App\Models\Airport;
 class SearchController extends Controller{
     
     public function search(Request $request){
-        
+
+
+        /**
+        *
+        *  Validate the request and mapping of arguments
+        *
+        */
+
         $data = request()->validate([
             'departure' => 'required|exists:App\Models\Airport,icao',
             'continent' => 'sometimes|string',
             'codeletter' => 'required|string',
-            'rwyLengthMin' => 'sometimes|between:0,16000',
-            'rwyLengthMax' => 'sometimes|between:0,16000',
-            'airtimeMin' => 'sometimes|between:0,24',
-            'airtimeMax' => 'sometimes|between:0,24',
-            'elevationMin' => 'sometimes|between:-2000,18000',
-            'elevationMax' => 'sometimes|between:-2000,18000',
+            'airtimeMin' => 'sometimes|numeric|between:0,24',
+            'airtimeMax' => 'sometimes|numeric|between:0,24',
             'scores' => 'sometimes|array',
-            'metcondition' => 'sometimes|in:IFR,VFR',
+            'metcondition' => 'sometimes|in:IFR,VFR,ANY',
+            'destinationWithRoutesOnly' => 'sometimes|numeric|between:-1,1',
+            'destinationRunwayLights' => 'sometimes|numeric|between:-1,1',
+            'destinationAirbases' => 'sometimes|numeric|between:-1,1',
+            'destinationAirportSize' => 'sometimes|array',
+            'elevationMin' => 'sometimes|numeric|between:-2000,18000',
+            'elevationMax' => 'sometimes|numeric|between:-2000,18000',
+            'rwyLengthMin' => 'sometimes|numeric|between:0,17000',
+            'rwyLengthMax' => 'sometimes|numeric|between:0,17000',
+            
             'arrivalWhitelist' => 'sometimes|array',
             'limit' => 'sometimes|integer|between:1,30',
-            //'onlyAirportsWithScore' => 'sometimes|boolean'
         ]);
 
         isset($data['continent']) ? $continent = $data['continent'] : $continent = null;
         $codeletter = $data['codeletter'];
-        isset($data['rwyLengthMin']) ? $rwyLengthMin = $data['rwyLengthMin'] : $rwyLengthMin = 0;
-        isset($data['rwyLengthMax']) ? $rwyLengthMax = $data['rwyLengthMax'] : $rwyLengthMax = 16000;
         isset($data['airtimeMin']) ? $airtimeMin = $data['airtimeMin'] : $airtimeMin = 0;
         isset($data['airtimeMax']) ? $airtimeMax = $data['airtimeMax'] : $airtimeMax = 24;
+        isset($data['scores']) ? $filterByScores = $data['scores'] : $filterByScores = null;
+        isset($data['metcondition']) ? $metcon = $data['metcondition'] : $metcon = null;
+        isset($data['destinationWithRoutesOnly']) ? $destinationWithRoutesOnly = (int)$data['destinationWithRoutesOnly'] : $destinationWithRoutesOnly = null;
+        isset($data['destinationRunwayLights']) ? $destinationRunwayLights = (int)$data['destinationRunwayLights'] : $destinationRunwayLights = null;
+        isset($data['destinationAirbases']) ? $destinationAirbases = (int)$data['destinationAirbases'] : $destinationAirbases = null;
+        (isset($data['destinationAirportSize']) && !empty($data['destinationAirportSize'])) ? $destinationAirportSize = $data['destinationAirportSize'] : $destinationAirportSize = null;
         isset($data['elevationMin']) ? $elevationMin = $data['elevationMin'] : $elevationMin = -2000;
         isset($data['elevationMax']) ? $elevationMax = $data['elevationMax'] : $elevationMax = 18000;
-        isset($data['scores']) ? $filteredScores = $data['scores'] : $filteredScores = null;
-        isset($data['metcondition']) ? $metcon = $data['metcondition'] : $metcon = null;
+        isset($data['rwyLengthMin']) ? $rwyLengthMin = $data['rwyLengthMin'] : $rwyLengthMin = 0;
+        isset($data['rwyLengthMax']) ? $rwyLengthMax = $data['rwyLengthMax'] : $rwyLengthMax = 17000;
+        
         isset($data['arrivalWhitelist']) ? $arrivalWhitelist = $data['arrivalWhitelist'] : $arrivalWhitelist = null;
         isset($data['limit']) ? $resultLimit = $data['limit'] : $resultLimit = 10;
-        //(isset($data['onlyAirportsWithScore']) && (boolean)$data['onlyAirportsWithScore']) ? $onlyAirportsWithScore = true : $onlyAirportsWithScore = false;
+
+        // Fetch the requested score filtering and cast it to numbers
+        if(isset($filterByScores)){
+            $filterByScores = collect($filterByScores)->map(function($item, $key){
+                return (int)$item;
+            });
+        }
+
+
+        /**
+        *
+        *  Fetch the requested data
+        *
+        */
 
         $departure = Airport::where('icao', $data['departure'])->get()->first();
 
         $airports = collect();
-        $airports = Airport::findWithCriteria($continent, $departure->iso_country, $departure->icao, $arrivalWhitelist);
+        $airports = Airport::findWithCriteria($continent, $departure->iso_country, $departure->icao, $destinationAirportSize, $arrivalWhitelist, $filterByScores, $destinationRunwayLights, $destinationAirbases, $destinationWithRoutesOnly);
 
-        $suggestedAirports = $airports->filterWithCriteria($departure, $codeletter, $airtimeMin, $airtimeMax, $metcon, $filteredScores, $rwyLengthMin, $rwyLengthMax, $elevationMin, $elevationMax);
+        $suggestedAirports = $airports->filterWithCriteria($departure, $codeletter, $airtimeMin, $airtimeMax, $metcon, $rwyLengthMin, $rwyLengthMax, $elevationMin, $elevationMax);
         $suggestedAirports = $suggestedAirports->shuffle(); 
-        
-        // @TODO: Implement once strict search is availble. If requested result with only scored airports
-        /*if($onlyAirportsWithScore){
-            $suggestedAirports = $suggestedAirports->filter(function($airport){
-                return $airport->scores->count() == 0;
-            });
-        }*/
-        
-        $suggestedAirports = $suggestedAirports->sortByFilteredScores($filteredScores);
+
+        $suggestedAirports = $suggestedAirports->sortByScores($filterByScores);
         $suggestedAirports = $suggestedAirports->splice(0,$resultLimit);
 
-        // Prepare arrival suggestions
+        /**
+        *
+        *  Prepare the data for the response
+        *
+        */
         $arrivalData = collect();
         foreach($suggestedAirports as $airport){
 
