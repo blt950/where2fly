2915332325c1b6d15e847945c1c991a8b006f184
Daniel <blt950@users.noreply.github.com>
2023-09-09 10:13:53 +0200

    Fixed broken random search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -100,7 +100,7 @@ class SearchController extends Controller
             $departure = Airport::where('icao', $data['departure'])->get()->first();
         } else {
             // Get a random airport from the toplist
-            $departure = Airport::findWithCriteria($continent)->sortByFilteredScores($filterByScores)->slice(0, 10)->random();
+            $departure = Airport::findWithCriteria($continent)->sortByScores($filterByScores)->slice(0, 10)->random();
             $suggestedDeparture = true;
         }
 
