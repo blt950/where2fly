a9d461c8242ea5eb0161a38588b764e1cdd8e488
Daniel <blt950@users.noreply.github.com>
2024-04-01 13:37:34 +0200

    fix: moved discord to part

--- resources/views/search/airports.blade.php
+++ resources/views/search/airports.blade.php
@@ -268,11 +268,7 @@
 
             </div>
 
-            <div class="alert alert-discord">
-                <p class="text-white mb-0">
-                    <i class="fa-brands fa-discord"></i> Contribute with suggestions, bug reports and vote on new features in <a class="text-white" href="https://discord.gg/UkFg9Yy4gP" target="_blank">our Discord <i class="fas fa-up-right-from-square"></i></a>
-                </p>
-            </div>
+            @include('parts.discord')
         </div>
 
         @include('layouts.legend')
