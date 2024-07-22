2c387bdf4fb263823ac6b4e875a9350ffb3abcf5
Daniel <blt950@users.noreply.github.com>
2024-07-22 10:27:54 +0200

    feat: upgrade from Laravel 9 -> 11 (#94)
    * feat: laravel 10
    
    * chore: update to php 8.3 and node 22
    
    * feat: Laravel 11
    
    * chore: updated npm

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:20,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['middleware' => ['api-token']], function () {
     Route::get('/top', [App\Http\Controllers\API\TopController::class, 'index'])->name('api.top.index');
     Route::post('/top', [App\Http\Controllers\API\TopController::class, 'indexWhitelist'])->name('api.top.index.whitelist');
