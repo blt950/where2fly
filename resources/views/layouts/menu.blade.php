dd415428e4f89f74403cffe980699425b396f8c6
Daniel <blt950@users.noreply.github.com>
2024-08-14 20:03:07 +0200

    responsive menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,18 +1,35 @@
 <header class="mb-auto">
-    <div>
-        <a href="{{ route('front') }}" class="float-none float-md-start text-decoration-none text-white">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a href="{{ route('front') }}" class="navbar-brand">
             <h3 class="mb-0 logo">Where2Fly</h3>
         </a>
-    
-        <nav class="nav nav-masthead justify-content-center float-md-end">
-            <a class="nav-link {{ Route::is('front*') || Route::is('search*') ? 'active' : '' }}" href="{{ route('front') }}">Search</a>
-            <a class="nav-link {{ Route::is('top*') ? 'active' : '' }}" href="{{ route('top') }}">Top List</a>
-            <a class="nav-link {{ Route::is('changelog') ? 'active' : '' }}" href="{{ route('changelog') }}">Changelog</a>
-            <a class="nav-link d-none d-md-block" href="https://forms.gle/wsP3s322LTP6oJog8" target="_blank">Feedback</a>
-            <a class="nav-link" href="https://discord.gg/UkFg9Yy4gP" target="_blank">Discord</a>
-            <a class="nav-link" href="#" target="_blank">Donate</a>
-        </nav>
-    </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::is('front*') || Route::is('search*') ? 'active' : '' }}" href="{{ route('front') }}">Search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::is('top*') ? 'active' : '' }}" href="{{ route('top') }}">Top List</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Route::is('changelog') ? 'active' : '' }}" href="{{ route('changelog') }}">Changelog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://forms.gle/wsP3s322LTP6oJog8" target="_blank">Feedback</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://discord.gg/UkFg9Yy4gP" target="_blank">Discord</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" target="_blank">Donate</a>
+                </li>
+            </ul>
+        </div>
+        <div id="menu-overlay" class="menu-overlay d-md-none"></div>
+    </nav>
 </header>
 @if(Config::get('app.env') != "production")
     <span class="testbadge badge bg-danger  ms-2 mt-2" role="alert">
