f997921c579d31d7d6fa15402f3c5542675f481b
Daniel <blt950@users.noreply.github.com>
2024-08-24 22:57:03 +0200

    fix: better visibility airline aircraft highlight

--- resources/views/parts/flightsCard.blade.php
+++ resources/views/parts/flightsCard.blade.php
@@ -10,7 +10,7 @@
 
     <ul class="list-unstyled">
         @foreach($airport->flights->where('airline_icao', $airline->icao_code)->sortByDesc('last_seen_at') as $flight)
-            <li class="{{$flight->aircrafts->whereIn('icao', $filterByAircrafts)->count() > 0 ? 'highlight' : null}}">
+            <li class="{{$flight->aircrafts->whereIn('icao', $filterByAircrafts)->count() > 0 ? 'text-success' : null}}">
                 {{ $flight->flight_icao }}
                 ({{ $flight->aircrafts->pluck('icao')->implode(',') }})
                 {{ $flight->last_seen_at->diffForHumans() }}
