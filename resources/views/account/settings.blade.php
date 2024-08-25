96f13a67bac64315a1ecb19273698ede42892d3d
Daniel <blt950@users.noreply.github.com>
2024-08-25 09:59:27 +0200

    fix: missing account recovery link in account settings

--- resources/views/account/settings.blade.php
+++ resources/views/account/settings.blade.php
@@ -6,7 +6,11 @@
     @include('layouts.title', ['title' => 'Account Settings'])
 
     <div class="container">
-        <h2>Delete account</h2>
+        <h2>Reset password</h2>
+        <p class="font-work-sans">You may reset you password by performing an account reset. Your data will <b>not</b> be lost.</p>
+        <a href="{{ route('account.recovery') }}" class="btn btn-primary">Reset password</a>
+
+        <h2 class="mt-4">Delete account</h2>
         <p class="font-work-sans">Deleting your account will remove all your data from the system. This action is irreversible.</p>
         <form method="POST" action="{{ route('user.delete') }}">
             @csrf
