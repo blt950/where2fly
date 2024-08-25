48cc8e42880089a2ceb6869ec06d7dc1d8e05a45
Daniel <blt950@users.noreply.github.com>
2024-08-25 12:28:08 +0200

    chore: QoL

--- resources/views/front/parts/tabs.blade.php
+++ resources/views/front/parts/tabs.blade.php
@@ -1,5 +1,5 @@
 <div class="btn-group filter" style="text-align: left; padding-bottom: 2rem;">
     <a href="{{ @route('front') }}" class="btn btn-outline-primary {{ Route::is('front') ? 'active' : '' }}"><i class="fas fa-plane-arrival"></i> Find Arrival</a>
     <a href="{{ @route('front.departures') }}" class="btn btn-outline-primary {{ Route::is('front.departures') ? 'active' : '' }}"><i class="fas fa-plane-departure"></i> Find Departure</a>
-    <a href="{{ @route('front.routes') }}" class="btn btn-outline-primary {{ Route::is('front.routes') ? 'active' : '' }}"><i class="fas fa-route"></i> Find Routes</a>
+    <a href="{{ @route('front.routes') }}" class="btn btn-outline-primary {{ Route::is('front.routes') ? 'active' : '' }}"><i class="fas fa-route"></i> Find Flights</a>
 </div>
