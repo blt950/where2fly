9cb30d6e2394d1983317f4059ba37efd816e7cdd
Daniel <blt950@users.noreply.github.com>
2024-11-16 21:11:41 +0100

    feat: umami tracking test

--- resources/views/layouts/tracking.blade.php
+++ resources/views/layouts/tracking.blade.php
@@ -1,8 +1,10 @@
 @if(config('app.env') == 'production')
     <script defer data-domain="where2fly.today" src="{{ (isset($manualTracking) && $manualTracking) ? 'https://metrics.blt950.com/js/script.manual.js' : 'https://metrics.blt950.com/js/script.js' }}"></script>
+    <script defer src="https://umami.blt950.com/script.js" data-website-id="2e068f9a-3125-41cd-aeba-b1cd74f20fd4"></script>
 @else
     <meta name="robots" content="noindex">
     <script defer data-domain="qa.where2fly.today" src="{{ (isset($manualTracking) && $manualTracking) ? 'https://metrics.blt950.com/js/script.manual.js' : 'https://metrics.blt950.com/js/script.js' }}"></script>
+    <script defer src="https://umami.blt950.com/script.js" data-website-id="f9d5bb7d-b528-4c7c-8a85-5a401136ee29"></script>
 @endif
 
 @isset($manualTracking)
