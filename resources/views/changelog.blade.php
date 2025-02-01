989f9de2fcf937805130837dbba457f94daf324b
Daniel <blt950@users.noreply.github.com>
2025-02-01 22:52:58 +0100

    fix: version bump

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -12,6 +12,12 @@
     <div class="container">
         <div class="text-start">
 
+            <h2 class="mb-0">v2.5.2</h2>
+            <span class="badge ps-0 pb-2">2025-02-02</span>
+            <ul>
+                <li>Fixed SimMarket links</li>
+            </ul>
+
             <h2 class="mb-0">v2.5.1</h2>
             <span class="badge ps-0 pb-2">2025-02-02</span>
             <ul>
