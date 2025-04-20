021d1e01451ca61736afca4211d106140a0a01a1
Daniel <blt950@users.noreply.github.com>
2025-04-20 09:17:48 +0200

    fix(api): provide request origin when unauth

--- app/Http/Middleware/ApiToken.php
+++ app/Http/Middleware/ApiToken.php
@@ -29,7 +29,7 @@ class ApiToken
 
         if ($key == null || ($request->getClientIp() != $key->ip_address && $key->ip_address != '*')) {
             return response()->json([
-                'message' => 'Unauthorized',
+                'message' => 'Unauthorized. Request origin: ' . collect($request->getClientIps())->implode(', '),
             ], 401);
         }
 
