7a10351bce4fcce0ed0fb9c70a8129f62932666c
Daniel <blt950@users.noreply.github.com>
2025-07-31 21:01:02 +0200

    fix: close box upon selection

--- resources/js/functions/tags.js
+++ resources/js/functions/tags.js
@@ -1,42 +1,3 @@
-/*
-// Prevent adding tags that don't exist in u-option's and add hidden input to form
-document.querySelectorAll('u-tags').forEach(element => {
-    element.addEventListener('tags', (event) => {
-        const element = event.target;
-        const value = event.detail.item.value;
-        const options = Array.from(document.querySelectorAll('u-option')).map(option => option.value);
-
-        if (!options.includes(value)) {
-            event.preventDefault();
-        } else {
-            if (event.detail.action === 'add') {
-
-                // Create a hidden input element with the data-input-name and value equal to 'value'
-                const input = document.createElement('input');
-                input.type = 'hidden';
-                input.name = element.dataset.inputName;
-                input.value = value;
-                input.dataset.tagValue = value; // For easy identification later
-
-                // Append the hidden input the element
-                element.appendChild(input);
-            } else if (event.detail.action === 'remove') {
-                // Find the hidden input element with the corresponding value and remove it
-                const inputToRemove = element.querySelector(`input[type="hidden"][name="${element.dataset.inputName}"][value="${value}"]`);
-                if (inputToRemove) {
-                    inputToRemove.remove();
-                }
-            }
-
-            // Close the datalist on selection
-            const datalist = element.querySelector('u-datalist');
-            const input = element.querySelector('input');
-            input.focus();
-            datalist.hidden = true;
-        }
-    });
-})*/
-
 // Dynamically adjust placeholder for destinations
 document.querySelectorAll('u-combobox').forEach(element => {
     const input = element.querySelector('input[placeholder="Anywhere"]');
@@ -58,13 +19,16 @@ document.querySelectorAll('u-combobox').forEach(element => {
     }
 })
 
-// Clear input field after selection
+// Clean input field and close the box upon selection
 document.querySelectorAll('u-combobox').forEach(element => {
     const input = element.querySelector('input');
+    const datalist = element.querySelector('u-datalist');
 
     if(input){
         element.addEventListener('afterchange', (event) => {
             input.value = null;
+            input.focus();
+            datalist.hidden = true;
         });
     }
 })
