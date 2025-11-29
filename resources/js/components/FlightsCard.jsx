b4e2001098a4fd61b233d9da8673563eceef475b
Daniel <blt950@users.noreply.github.com>
2025-11-29 06:26:15 +0100

    fix: links in jsx

--- resources/js/components/FlightsCard.jsx
+++ resources/js/components/FlightsCard.jsx
@@ -7,9 +7,9 @@ function FlightsCard({ airlineId, departureAirportId, arrivalAirportId }) {
 
     const dataCache = useRef({});
     const [data, setData] = useState(null);
-    const { highlightedAircrafts } = useContext(MapContext);
+    const { airports, primaryAirport, focusAirport, highlightedAircrafts } = useContext(MapContext);
     const { setShowFlightsIdCard } = useContext(CardContext);
-
+    
     // Fetch airport data if it's not in the cache
     useEffect(() => {
         if (dataCache.current[airlineId]) {
@@ -51,28 +51,64 @@ function FlightsCard({ airlineId, departureAirportId, arrivalAirportId }) {
                     <table className="table card-table no-padding sortable asc">
                         <thead>
                             <tr>
-                                <th scope="col" style={{'maxWidth': '25%'}}>Flight</th>
-                                <th scope="col" style={{'width': '50%'}}>Aircraft</th>
-                                <th scope="col" style={{'maxWidth': '25%'}}>Last seen</th>
+                                <th scope="col" style={{maxWidth: '25%'}}>Flight</th>
+                                <th scope="col" style={{width: '50%'}}>Aircraft</th>
+                                <th scope="col" style={{maxWidth: '25%'}}>Last seen</th>
                             </tr>
                         </thead>
                         <tbody>
                             {data.flights.map(flight => (
                                 <tr key={flight.id}>
                                     <td className={flight.highlighted ? 'text-flight-success' : ''} data-sort={flight.flight_icao}>
-                                        
                                         <div className="dropdown fs-6 text-info link-underline-info link-underline-opacity-25-hover font-work-sans ps-0">
                                             <button className="btn btn-xs btn-xs-light dropdown-toggle font-work-sans" type="button" data-bs-toggle="dropdown" aria-expanded="false">{flight.flight_icao}</button>
                                             <ul className="dropdown-menu">
-                                                <li><a className="dropdown-item" href="https://dispatch.simbrief.com/options/custom?orig={{ $departure->icao }}&dest={{ $arrival->icao }}&airline={{ $route->airline->icao_code }}&fltnum={{ $route->flight_number }}" target="_blank">Simbrief</a></li>
-                                                <li><a className="dropdown-item" href="https://www.flightradar24.com/data/flights/{{ strtolower($route->airline->iata_code . $route->flight_number) }}" rel="noreferrer" target="_blank">Flightradar24</a></li>
-                                                <li><a className="dropdown-item" href="https://www.flightaware.com/live/flight/{{ strtolower($route->airline->icao_code . $route->flight_number) }}" target="_blank">FlightAware</a></li>
-                                                <li><a className="dropdown-item" href="https://www.airnavradar.com/data/flights/{{ strtolower($route->airline->iata_code . $route->flight_number) }}" target="_blank">AirNavRadar</a></li>
+                                                <li>
+                                                    <a
+                                                        className="dropdown-item"
+                                                        href={`https://dispatch.simbrief.com/options/custom?orig=${airports[primaryAirport].icao}&dest=${airports[focusAirport].icao}&airline=${data.airline.icao_code}&fltnum=${flight.flight_number}`}
+                                                        target="_blank"
+                                                        rel="noreferrer"
+                                                    >
+                                                        Simbrief
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        className="dropdown-item"
+                                                        href={`https://www.flightradar24.com/data/flights/${(data.airline.iata_code + flight.flight_number).toLowerCase()}`}
+                                                        target="_blank"
+                                                    >
+                                                        Flightradar24
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        className="dropdown-item"
+                                                        href={`https://www.flightaware.com/live/flight/${(data.airline.icao_code + flight.flight_number).toLowerCase()}`}
+                                                        target="_blank"
+                                                    >
+                                                        FlightAware
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        className="dropdown-item"
+                                                        href={`https://www.airnavradar.com/data/flights/${(data.airline.iata_code + flight.flight_number).toLowerCase()}`}
+                                                        target="_blank"
+                                                    >
+                                                        AirNavRadar
+                                                    </a>
+                                                </li>
                                             </ul>
                                         </div>
                                     </td>
-                                    <td className={flight.highlighted ? 'text-flight-success' : ''}>{flight.aircrafts.map(aircraft => aircraft.icao).join(', ')}</td>
-                                    <td className={flight.highlighted ? 'text-flight-success' : ''} data-sort={flight.last_seen_at}>{moment(flight.last_seen_at).fromNow()}</td>
+                                    <td className={flight.highlighted ? 'text-flight-success' : ''}>
+                                        {flight.aircrafts.map(aircraft => aircraft.icao).join(', ')}
+                                    </td>
+                                    <td className={flight.highlighted ? 'text-flight-success' : ''} data-sort={flight.last_seen_at}>
+                                        {moment(flight.last_seen_at).fromNow()}
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
@@ -83,7 +119,7 @@ function FlightsCard({ airlineId, departureAirportId, arrivalAirportId }) {
             )}
             </div>
         </>
-    )
+    );
 }
 
 export default FlightsCard;
