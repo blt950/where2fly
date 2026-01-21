58b240e50ef7feaff099c8c041203145fcad356c
Daniel <blt950@users.noreply.github.com>
2026-01-21 21:13:59 +0100

    fix: Update tracking script source URLs to metrics.blt950.com

--- resources/views/layouts/tracking.blade.php
+++ resources/views/layouts/tracking.blade.php
@@ -1,6 +1,6 @@
 @if(config('app.env') == 'production')
-    <script defer src="https://umami.blt950.com/script.js" data-website-id="{{ config('umami.website_id_prod') }}" data-domains="where2fly.today" @isset($manualTracking) data-auto-track="false" @endisset></script>
+    <script defer src="https://metrics.blt950.com/script.js" data-website-id="{{ config('umami.website_id_prod') }}" data-domains="where2fly.today" @isset($manualTracking) data-auto-track="false" @endisset></script>
 @else
     <meta name="robots" content="noindex">
-    <script defer src="https://umami.blt950.com/script.js" data-website-id="{{ config('umami.website_id_dev') }}" @isset($manualTracking) data-auto-track="false" @endisset></script>
+    <script defer src="https://metrics.blt950.com/script.js" data-website-id="{{ config('umami.website_id_dev') }}" @isset($manualTracking) data-auto-track="false" @endisset></script>
 @endif
