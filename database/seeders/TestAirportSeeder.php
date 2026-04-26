fabdea7139561c1aafb7a7d333060d121b25ca48
Daniel <blt950@users.noreply.github.com>
2026-04-26 10:53:52 +0200

    fix: improved with some more seeds

--- database/seeders/TestAirportSeeder.php
+++ database/seeders/TestAirportSeeder.php
@@ -22,6 +22,15 @@ class TestAirportSeeder extends Seeder
             ['icao' => 'EHAM', 'name' => 'Amsterdam Schiphol',           'lat' => 52.3086, 'lon' => 4.7639, 'type' => 'large_airport', 'iso_country' => 'NL', 'iso_region' => 'NL-NH',  'continent' => 'EU', 'municipality' => 'Amsterdam'],
             ['icao' => 'LFPG', 'name' => 'Paris Charles de Gaulle',      'lat' => 49.0128, 'lon' => 2.5500, 'type' => 'large_airport', 'iso_country' => 'FR', 'iso_region' => 'FR-IDF', 'continent' => 'EU', 'municipality' => 'Paris'],
             ['icao' => 'RJTT', 'name' => 'Tokyo Haneda',                 'lat' => 35.5494, 'lon' => 139.7798, 'type' => 'large_airport', 'iso_country' => 'JP', 'iso_region' => 'JP-13',  'continent' => 'AS', 'municipality' => 'Tokyo'],
+            ['icao' => 'EDDS', 'name' => 'Stuttgart Airport',            'lat' => 48.69,   'lon' => 9.22,    'type' => 'large_airport',  'iso_country' => 'DE', 'iso_region' => 'DE-BW',  'continent' => 'EU', 'municipality' => 'Stuttgart', 'elevation_ft' => 1276],
+            ['icao' => 'EDDB', 'name' => 'Berlin Brandenburg Airport',   'lat' => 52.36,   'lon' => 13.50,   'type' => 'large_airport',  'iso_country' => 'DE', 'iso_region' => 'DE-BR',  'continent' => 'EU', 'municipality' => 'Berlin',    'elevation_ft' => 157],
+            ['icao' => 'ENBR', 'name' => 'Bergen Airport, Flesland',     'lat' => 60.29,   'lon' => 5.22,    'type' => 'large_airport',  'iso_country' => 'NO', 'iso_region' => 'NO-46',  'continent' => 'EU', 'municipality' => 'Bergen',    'elevation_ft' => 170],
+            ['icao' => 'ENGM', 'name' => 'Oslo Airport, Gardermoen',     'lat' => 60.19,   'lon' => 11.10,   'type' => 'large_airport',  'iso_country' => 'NO', 'iso_region' => 'NO-32',  'continent' => 'EU', 'municipality' => 'Oslo',      'elevation_ft' => 681],
+            ['icao' => 'ENSO', 'name' => 'Stord Airport, Sørstokken',    'lat' => 59.79,   'lon' => 5.34,    'type' => 'medium_airport', 'iso_country' => 'NO', 'iso_region' => 'NO-46',  'continent' => 'EU', 'municipality' => 'Leirvik',   'elevation_ft' => 160],
+            ['icao' => 'ENTC', 'name' => 'Tromsø Airport, Langnes',      'lat' => 69.68,   'lon' => 18.92,   'type' => 'large_airport',  'iso_country' => 'NO', 'iso_region' => 'NO-55',  'continent' => 'EU', 'municipality' => 'Tromsø',    'elevation_ft' => 31],
+            ['icao' => 'ENTO', 'name' => 'Sandefjord Airport, Torp',     'lat' => 59.19,   'lon' => 10.26,   'type' => 'medium_airport', 'iso_country' => 'NO', 'iso_region' => 'NO-39',  'continent' => 'EU', 'municipality' => 'Torp',      'elevation_ft' => 286],
+            ['icao' => 'ENSG', 'name' => 'Sogndal Airport, Haukåsen',    'lat' => 61.16,   'lon' => 7.14,    'type' => 'small_airport',  'iso_country' => 'NO', 'iso_region' => 'NO-46',  'continent' => 'EU', 'municipality' => 'Sogndal',   'elevation_ft' => 1633],
+            ['icao' => 'ENSD', 'name' => 'Sandane Airport, Anda',        'lat' => 61.83,   'lon' => 6.11,    'type' => 'small_airport',  'iso_country' => 'NO', 'iso_region' => 'NO-46',  'continent' => 'EU', 'municipality' => 'Sandane',   'elevation_ft' => 196],
         ];
 
         foreach ($airports as $data) {
@@ -36,7 +45,7 @@ class TestAirportSeeder extends Seeder
                 'iso_country' => $data['iso_country'],
                 'iso_region' => $data['iso_region'],
                 'municipality' => $data['municipality'],
-                'elevation_ft' => 100,
+                'elevation_ft' => $data['elevation_ft'] ?? 100,
                 'scheduled_service' => 'yes',
                 'coordinates' => new Point($data['lat'], $data['lon'], Srid::WGS84->value),
             ]);
