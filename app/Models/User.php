45d9c45817fa06dcec7ed1526db68eb2d1e34cdb
Daniel <blt950@users.noreply.github.com>
2026-04-26 16:05:01 +0200

    fix: throttle user activity property

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'last_activity_at' => 'datetime',
     ];
 
     public function getAirportsFromLists()
