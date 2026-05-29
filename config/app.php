7b226f8f9336d9304568b15370034d67b6682df7
Daniel <blt950@users.noreply.github.com>
2026-05-29 21:53:36 +0200

    refactor: providers

--- config/app.php
+++ config/app.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-use GrahamCampbell\Markdown\MarkdownServiceProvider;
-use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
-
 return [
 
     /*
@@ -178,36 +173,5 @@ return [
         'driver' => 'file',
         // 'store'  => 'redis',
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        AppServiceProvider::class,
-        MarkdownServiceProvider::class,
-    ])->toArray(),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Class Aliases
-    |--------------------------------------------------------------------------
-    |
-    | This array of class aliases will be registered when this application
-    | is started. However, feel free to register as many as you wish as
-    | the aliases are "lazy" loaded so they don't hinder performance.
-    |
-    */
-
-    'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
-    ])->toArray(),
 
 ];
