6c5df424903682d0fe51258424cbe76be439602b
Daniel <blt950@users.noreply.github.com>
2026-06-22 09:33:40 +0200

    fix: dropdown colors

--- resources/js/components/ui/CurrencyDropdown.jsx
+++ resources/js/components/ui/CurrencyDropdown.jsx
@@ -2,7 +2,7 @@ function CurrencyDropdown({ currencies, currency, setCurrency }) {
     return (
         <>
             <div className="btn-group">
-                <button className="btn btn-sm mb-2 btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <button className="btn btn-sm mb-2 btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     {currencies.find(c => c.code === currency).code} {currencies.find(c => c.code === currency).symbol}
                 </button>
                 <ul className="dropdown-menu">
