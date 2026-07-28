33aa23e3831dbf6cb57ed1593c43118080f2b231
Daniel <blt950@users.noreply.github.com>
2026-07-28 12:04:54 +0200

    fix: turkey country search

--- app/Helpers/CountryHelper.php
+++ app/Helpers/CountryHelper.php
@@ -6,8 +6,8 @@ use Symfony\Component\Intl\Countries;
 
 class CountryHelper
 {
-    /** Codes not in the official ISO set, so symfony/intl doesn't know them */
-    private const OVERRIDES = ['XK' => 'Kosovo'];
+    private const ADDITIONS = ['XK' => 'Kosovo'];
+    private const OVERRIDES = ['TR' => 'Türkiye (Turkey)'];
 
     public const US_STATES = ['AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona', 'AR' => 'Arkansas', 'CA' => 'California', 'CO' => 'Colorado', 'CT' => 'Connecticut', 'DE' => 'Delaware', 'FL' => 'Florida', 'GA' => 'Georgia', 'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois', 'IN' => 'Indiana', 'IA' => 'Iowa', 'KS' => 'Kansas', 'KY' => 'Kentucky', 'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland', 'MA' => 'Massachusetts', 'MI' => 'Michigan', 'MN' => 'Minnesota', 'MS' => 'Mississippi', 'MO' => 'Missouri', 'MT' => 'Montana', 'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire', 'NJ' => 'New Jersey', 'NM' => 'New Mexico', 'NY' => 'New York', 'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio', 'OK' => 'Oklahoma', 'OR' => 'Oregon', 'PA' => 'Pennsylvania', 'RI' => 'Rhode Island', 'SC' => 'South Carolina', 'SD' => 'South Dakota', 'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont', 'VA' => 'Virginia', 'WA' => 'Washington', 'WV' => 'West Virginia', 'WI' => 'Wisconsin', 'WY' => 'Wyoming'];
 
@@ -16,16 +16,21 @@ class CountryHelper
      */
     public static function names(): array
     {
-        $names = Countries::getNames('en') + self::OVERRIDES;
+        $names = Countries::getNames('en') + self::ADDITIONS;
         asort($names);
+
+        // Manual overrides
+        foreach(self::OVERRIDES as $country => $override){
+            $names[$country] = $override;
+        }
 
         return $names;
     }
 
     public static function name(?string $code): string
     {
-        if (isset(self::OVERRIDES[$code])) {
-            return self::OVERRIDES[$code];
+        if (isset(self::ADDITIONS[$code])) {
+            return self::ADDITIONS[$code];
         }
 
         return $code !== null && Countries::exists($code) ? Countries::getName($code, 'en') : 'Unknown Country';
