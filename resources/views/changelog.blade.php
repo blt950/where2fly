c754cad6b3383b51ce8cc91a152ff76066488ac6
Daniel <blt950@users.noreply.github.com>
2026-07-28 10:47:49 +0200

    fix: increased rem's

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -32,6 +32,7 @@
                 <li>Fixed airport ICAO not always displaying on the map when selecting an suggestion</li>
                 <li>Fixed stuck tooltips not disappearing when clicked on</li>
                 <li>Fixed scenery tabs order to show MSFS2024 first</li> 
+                <li>Fixed some design breaking when using increased font size</li>
                 <li>A lot of changes under the hood for stability and performance. Searches are now up to 4x faster!</li>
                 <li>API: Added more data rich scores return with predictions</li>
             </ul>
