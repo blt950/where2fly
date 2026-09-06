7a5ee2538785f2cc5c2a8741f3a63c9f44b264ec
Daniel <2505044+blt950@users.noreply.github.com>
2026-09-06 15:26:36 +0000

    docs: changelog

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -12,6 +12,12 @@
     <div class="container">
         <div class="text-start">
 
+            <h2 class="mb-0">v3.1.3</h2>
+            <span class="badge ps-0 pb-2">2026-09-06</span>
+            <ul>
+                <li>Fixed long named lists in map options breaking the design</li>
+            </ul>
+
             <h2 class="mb-0">v3.1.2</h2>
             <span class="badge ps-0 pb-2">2026-09-01</span>
             <ul>
